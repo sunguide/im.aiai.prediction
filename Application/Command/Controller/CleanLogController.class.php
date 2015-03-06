7861da2354436787d9f0fd96a2c868857e013cf2
sunguide <sunguide@qq.com>
2015-03-07 01:07:11 +0800

    清楚日志

--- Application/Command/Controller/CleanLogController.class.php
+++ Application/Command/Controller/CleanLogController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class CleanLogController extends CrontabController {
 
     public function clean(){
-        $logs =  C('LOG_PATH')."/Commands/".date('y_m_d_',time() - 864000);
+        $logs =  C('LOG_PATH')."/Commands/*".date('y_m_d_',time() - 864000);
         $cmd = "rm ".$logs."*";
         exec($cmd);
     }
