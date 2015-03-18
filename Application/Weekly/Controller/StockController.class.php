0ad942c4a5603bea2f1b45d3b464cad96f51dda4
sunguide <sunguide@qq.com>
2015-03-18 18:07:20 +0800

    修改邮件发送系统，增加发送人

---
+++ Application/Weekly/Controller/StockController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Weekly\Controller;
+use Think\Controller;
+class StockController extends Controller {
+
+    public function detail(){
+        $id = intval(I("id"));
+        $PositionModel = M("Position");
+        $item = $PositionModel->find($id);
+        $this->assign("item", $item);
+        $this->display();
+    }
+    public function index(){
+        $StockModel = M("Stock");
+    }
+
+}
