fd35849544796cb04651dd6c55b8c0ab9363f444
sunguide <sunguide@qq.com>
2015-03-19 15:55:00 +0800

    修改邮件发送系统，增加发送人

--- Application/Command/Working/EmailWorking.class.php
+++ Application/Command/Working/EmailWorking.class.php
@@ -9,11 +9,15 @@ namespace Command\Working;
 use  Common\Service\EmailService;
 class EmailWorking extends QueueWorking {
 
+    private $frequencyInterval = 3600;
     public function getQueueName(){
         return QUEUE_EMAIL;
     }
 
     protected function working($data = array()){
+        if(!$this->checkFrequency($data)){
+            return true;
+        }
         $to = isset($data['to']) ? $data['to'] : "";
         $toName = isset($data['to_name']) ? $data['to_name'] : "";
         $title = isset($data['title']) ? $data['title'] : "";
@@ -22,5 +26,14 @@ class EmailWorking extends QueueWorking {
         $fromName = isset($data['from_name']) ? $data['from_name'] : "";
         EmailService::getInstance()->send($to,$toName,$title,$content,$from,$fromName);
     }
-
+    protected function checkFrequency($data){
+        $options = array('type'=>'file','prefix'=>'email_send','expire'=>$this->frequencyInterval);
+        $id = md5(json_encode($data));
+        if(S($id,"",$options)){
+            return false;
+        }else{
+            S($id,$data,$options);
+            return true;
+        }
+    }
 }
